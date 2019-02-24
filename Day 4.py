94985c1868be6e78924817750bdab27dbd2b3ace
Saqib Ali <pysaqib@gmail.com>
2019-02-24 16:37:37 +0500

    Added Day 4.py
    Finished the challenge Day 4

---
+++ Day 4.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+class Person:
+    def __init__(self,initialAge):
+        age = initialAge
+
+        if (age < 0):
+            age = 0
+            print("Age is not valid, setting age to 0.")
+
+    def amIOld(self):
+        if (age < 13):
+            print("You are young.")
+        elif (age >= 13 and age < 18):
+            print("You are a teenager.")
+        else:
+            print("You are old.")
+
+    def yearPasses(self):
+        global age
+        age += 1
+
+## Stub code by HackerRank ##
+t = int(input())
+for i in range(0, t):
+    age = int(input())         
+    p = Person(age)  
+    p.amIOld()
+    for j in range(0, 3):
+        p.yearPasses()       
+    p.amIOld()
+    print("")

6065bb79280b5c1b02024b3c31471d92730e82f1
Saqib Ali <pysaqib@gmail.com>
2019-03-26 02:41:51 +0500

    Challenge Completed.

---
+++ Day 27.java
@@ -0,0 +1,25 @@
+static class TestDataEmptyArray {
+    public static int[] get_array() {
+        // complete this function
+    }
+}
+
+static class TestDataUniqueValues {
+    public static int[] get_array() {
+        // complete this function
+    }
+
+    public static int get_expected_result() {
+        // complete this function
+    }
+}
+
+static class TestDataExactlyTwoDifferentMinimums {
+    public static int[] get_array() {
+        // complete this function
+    }
+
+    public static int get_expected_result() {
+        // complete this function
+    }
+}
